9a0fee689eb1d54585d18bda7d10490123359128
Kit <kittsville@googlemail.com>
2017-05-11 17:41:12 +0100

    Add Base64 ID URL creation/validation

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Generates 11 character Base64 ID
+ * @see https://en.wikipedia.org/wiki/Base64#URL_applications
+ *
+ * @return string Creates 
+ */
+function newId() {
+    return rtrim(strtr(base64_encode(openssl_random_pseudo_bytes(9)), '+/', '-_'), '=');
+}
+
+/**
+ * Checks if the string has the format of a valid ID
+ *
+ * @param  string $id The string to be validated
+ * @return bool       Whether string has the correct ID format
+ */
+function validId($id) {
+    return (bool) preg_match('/^[A-Za-z0-9\-_]{12}$/', $id);
+}
